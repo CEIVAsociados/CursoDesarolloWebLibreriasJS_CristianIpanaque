7958c21b21361a974dbfde6c3d1a795d568fbc26
CEIVAsociados <cristian030892@gmail.com>
2020-03-18 23:03:16 -0500

    timer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-//Cambiar el color del Titulo cada cierto tiempo
+var mov = 0;
+var puntos = 0;
+
+//1. Cambiar el color del Titulo cada cierto tiempo
 function cambiarColorTitulo(selector){
 	 $(selector).animate(
 	 {
@@ -21,11 +24,28 @@ function cambiarColorTitulo(selector){
 	 }).delay(1000);
 }
 
+//Iniciar el juego
+function iniciarJuego(){
+	$('.btn-reinicio').click(function () {
+		parar();
+		mov = 0;
+		time = 2;
+		puntos = 0;
+		$("#movimientos-text").text("0");
+		$("#score-text").text("0");
+		$("#timer").text("02:00")
+		$(this).text("Reiniciar");
+		//agregarDulcesPantalla();
+		iniciar();
+	});
+}
 //Cargar dulces aleatoriamente
 function cargarDulcesAleatorios() {
     return Math.floor(Math.random() * 4) + 1;
 }
 
-$(document).ready(function() {
+
+$(document).ready(function(){
   	cambiarColorTitulo(".main-titulo");
+  	iniciarJuego();
 });
